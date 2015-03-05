4f960d8841ab688e3925cbd3331ae71a41fbdefa
Á¤ÇöÁö(chunghj09) <chunghj09@infobank.net>
2015-03-05 15:18:36 +0900

    messageTong UI - pagerSlidingtap

---
+++ app/src/main/java/sample/info/minitong/MainActivity.java
@@ -0,0 +1,49 @@
+package sample.info.minitong;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.astuetz.PagerSlidingTabStrip;
+
+
+public class MainActivity extends FragmentActivity {
+
+    private PagerSlidingTabStrip mPagerTaps;
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mPagerTaps = (PagerSlidingTabStrip) this.findViewById(R.id.taps);
+        mViewPager = (ViewPager) this.findViewById(R.id.pager);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/sample/info/minitong/MainFragment.java
@@ -0,0 +1,7 @@
+package sample.info.minitong;
+
+/**
+ * Created by chunghj on 15. 3. 5..
+ */
+public class MainFragment  {
+}
